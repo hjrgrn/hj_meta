18a31b28b248d083cb375ac33c55f268a7030b7a
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-04-03 15:29:55 +0200

    Organize workflow in subcommands

--- src/main.rs
+++ src/main.rs
@@ -11,83 +11,102 @@ use std::{
     process::Command,
 };
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 const OUTPUT_DIR: &str = "./output/";
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-
-    if !cli.output_dir {
-        return Err(anyhow::anyhow!("Unimplemented."));
-    }
-
-    // TODO: configurable fields
-    let mut metadata = Vec::new();
-    metadata.push(Metadata::prompt("author")?);
-    metadata.push(Metadata::prompt("album")?);
-    metadata.push(Metadata::prompt("genre")?);
-
-    create_dir(OUTPUT_DIR)?;
-
-    for entry in read_dir(".")? {
-        // TODO: track number
-        let entry = entry?;
-        if entry.file_type()?.is_file() {
-            let old_path = entry
-                .file_name()
-                .into_string()
-                .map_err(|e| anyhow::anyhow!(format!("{:?}", e)))?;
-            let title = old_path.replace(":", "_");
-            // TODO: fix this when implementing output_dir
-            let new_path;
-            if cli.output_dir {
-                new_path = format!("{}{}", OUTPUT_DIR, title);
-            } else {
+    match cli.cmd {
+        Cmd::Meta {
+            track_number: _,
+            output_dir,
+        } => {
+            if !output_dir {
                 return Err(anyhow::anyhow!("Unimplemented."));
             }
 
-            let mut args = Vec::new();
-            args.push("-i".to_string());
-            args.push(old_path);
-            args.push("-y".to_string());
-            args.push("-c".to_string());
-            args.push("copy".to_string());
+            // TODO: configurable fields
+            let mut metadata = Vec::new();
+            metadata.push(Metadata::prompt("author")?);
+            metadata.push(Metadata::prompt("album")?);
+            metadata.push(Metadata::prompt("genre")?);
 
-            for entry in metadata.iter() {
-                entry.add_to_args(&mut args);
+            create_dir(OUTPUT_DIR)?;
+
+            for entry in read_dir(".")? {
+                // TODO: track number
+                let entry = entry?;
+                if entry.file_type()?.is_file() {
+                    let old_path = entry
+                        .file_name()
+                        .into_string()
+                        .map_err(|e| anyhow::anyhow!(format!("{:?}", e)))?;
+                    let title = old_path.replace(":", "_");
+                    // TODO: fix this when implementing output_dir
+                    let new_path;
+                    if output_dir {
+                        new_path = format!("{}{}", OUTPUT_DIR, title);
+                    } else {
+                        return Err(anyhow::anyhow!("Unimplemented."));
+                    }
+
+                    let mut args = Vec::new();
+                    args.push("-i".to_string());
+                    args.push(old_path);
+                    args.push("-y".to_string());
+                    args.push("-c".to_string());
+                    args.push("copy".to_string());
+
+                    for entry in metadata.iter() {
+                        entry.add_to_args(&mut args);
+                    }
+
+                    let title_metadata = Metadata::new("title", &title);
+                    title_metadata.add_to_args(&mut args);
+
+                    if output_dir {
+                        args.push(new_path);
+                    } else {
+                        args.push(title.clone());
+                    }
+
+                    let output = Command::new("/usr/bin/ffmpeg").args(args).output()?;
+                    if !output.status.success() {
+                        eprintln!("Could not write track {title}.");
+                    }
+                }
             }
 
-            let title_metadata = Metadata::new("title", &title);
-            title_metadata.add_to_args(&mut args);
-
-            if cli.output_dir {
-                args.push(new_path);
-            } else {
-                args.push(title.clone());
-            }
-
-            let output = Command::new("/usr/bin/ffmpeg").args(args).output()?;
-            if !output.status.success() {
-                eprintln!("Could not write track {title}.");
-            }
+            Ok(())
         }
+        Cmd::Split => Err(anyhow::anyhow!("Unimplemented")),
     }
-
-    Ok(())
 }
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(name = "HJMeta")]
 #[command(author = "hjrgrn <187955624+hjrgrn@users.noreply.github.com>")]
 // TODO:
 #[command(about = "TODO", long_about = None)]
 struct Cli {
-    #[arg(short, long, default_value = "true")]
-    track_number: bool,
+    #[command(subcommand)]
+    cmd: Cmd,
+}
 
-    #[arg(short, long, default_value = "true")]
-    output_dir: bool,
+#[derive(Debug, Subcommand)]
+enum Cmd {
+    #[command(arg_required_else_help = true)]
+    Meta {
+        #[arg(short, long, default_value = "true")]
+        track_number: bool,
+
+        #[arg(short, long, default_value = "true")]
+        output_dir: bool,
+    },
+
+    #[command(arg_required_else_help = true)]
+    Split,
 }
 
 // Struct that represents a metadata that can be added to the `ffmpeg` command.
