76b06a3c368a495a1def0c57f6ffc0d3e43e5a66
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-04-06 16:28:18 +0200

    global: small refactor
    - Improve cognitive complexity
    - Add comments

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,8 @@
-//! Cli
+//! CLI related utils.
 
 use clap::{Args, Parser, Subcommand};
 
+/// CLI.
 #[derive(Parser, Debug)]
 #[command(name = "HJMeta")]
 #[command(author = "hjrgrn <187955624+hjrgrn@users.noreply.github.com>")]
@@ -12,22 +13,42 @@ pub struct Cli {
     pub cmd: Cmd,
 }
 
+/// Subcommands.
 #[derive(Debug, Subcommand)]
 pub enum Cmd {
-    /// Add metadata to an album.
+    /// # `meta` subcommand
     ///
-    /// An album is supporsed to be a directory containing just the audio track of the album.
-    // TODO:
+    /// Adds metadata to an album.
+    /// An album is expected to be a directory containing only the audio files of the album.
+    ///
+    /// ## Note
+    ///
+    /// If the `track_number` flag is provided, audio files must follow this format:
+    /// ```text
+    /// ^[0-9]+_.+$
+    /// ```
+    /// Avoid non-ASCII characters to prevent unexpected issues.
     Meta(MetaArgs),
 
+    // TODO:
     Split,
 }
 
+/// CLI Arguments that can be passed to the `meta` subcommand.
 #[derive(Debug, Args)]
 pub struct MetaArgs {
+    /// Adds track numbers to the metadata.
+    ///
+    /// For this feature to work correctly, audio file names must follow this format:
+    /// ```text
+    /// ^[0-9]+_.+$
+    /// ```
     #[arg(short, long, default_value = "true")]
     pub track_number: bool,
 
+    /// Saves the edited files to the specified directory.
+    ///
+    /// If this flag is not provided, the original files will be deleted.
     #[arg(short, long)]
     pub output_dir: Option<String>,
 }
